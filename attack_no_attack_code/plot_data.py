5b39b263607c2619bd408d80010df6137bdbd7d4
bogdanaKolic <108514766+bogdanaKolic@users.noreply.github.com>
2024-02-29 15:57:58 +0100

    DELL plots

--- attack_no_attack_code/plot_data.py
+++ attack_no_attack_code/plot_data.py
@@ -8,7 +8,7 @@ Created on Thu Feb 29 13:00:37 2024
 
 files = ['attack', 'attack_memset', 'attack_ntmp', 'double_sided', 'no_flushing', 'random', 'rh']
 
-for f in files:
+""" for f in files:
     with open(f'{f}_sampled.dat', 'r') as file:
         events = {}
         for line in file.readlines():
@@ -29,4 +29,30 @@ for f in files:
                 for fn in range(7):
                     if f == files[fn]:
                         outfile.write(f'{fn+1} ')
-                outfile.write(f'{line[0]} {line[1][0]} {line[1][1]} {line[1][2]} {line [1][3]}\n')
+                outfile.write(f'{line[0]} {line[1][0]} {line[1][1]} {line[1][2]} {line [1][3]}\n') """
+           
+for f in files:
+    with open(f'{f}_load.dat', 'r') as file:
+        events = {}
+        for line in file.readlines():
+            if line[0] != '#' and line[0] != '\n':
+                fields = line.split()
+                if fields[1][0] != '<':
+                    count = 0
+                    i = 1
+                    while fields[i][0] in "0123456789":
+                        count = 1000*count + int(fields[i])
+                        i += 1
+                    if float(fields[0]) in events:
+                        events[float(fields[0])] += (count, )
+                    else:
+                        events[float(fields[0])] = (count, )
+        with open(f'{f}.dat', 'w') as outfile:
+            sorted_list = sorted(list(events.items()))
+            for line in sorted_list:
+                if len(line[1]) != 4:
+                    continue
+                for fn in range(7):
+                    if f == files[fn]:
+                        outfile.write(f'{fn+1} ')
+                outfile.write(f'{line[0]} {line[1][0]} {line[1][1]} {line[1][2]} {line [1][3]}\n') 
