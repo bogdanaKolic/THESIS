6acc5d3c11bd4fc21a66151efff3b28bad615e12
bogdanaKolic <108514766+bogdanaKolic@users.noreply.github.com>
2024-03-15 12:16:13 +0100

    ASUS sampling

--- PAPI_sampling/sample.sh
+++ PAPI_sampling/sample.sh
@@ -6,20 +6,20 @@ sudo make
 
 while [ $NUM_RUNS -gt 0 ]
 do
-    sudo ./attack_sampled >> error_asus_load.csv
-    sudo ./attack_benchmark >> error_asus_load.csv
-    sudo ./attack_ntmp_sampled >> error_asus_load.csv
-    sudo ./attack_ntmp_benchmark >> error_asus_load.csv
-    sudo ./no_flushing_sampled >> error_asus_load.csv
-    sudo ./no_flushing_benchmark >> error_asus_load.csv
-    sudo ./random_sampled >> error_asus_load.csv
-    sudo ./random_benchmark >> error_asus_load.csv
-    sudo ./rowhammer_test_sampled >> error_asus_load.csv
-    sudo ./rowhammer_test_benchmark >> error_asus_load.csv
-    sudo ./double_sided_rowhammer_sampled >> error_asus_load.csv
-    sudo ./double_sided_rowhammer_benchmark >> error_asus_load.csv
-    sudo ./stream_sampled >> error_asus_load.csv
-    sudo ./stream_benchmark >> error_asus_load.csv
+    sudo ./attack_sampled >> asus_load.csv
+    sudo ./attack_benchmark >> asus_load.csv
+    sudo ./attack_ntmp_sampled >> asus_load.csv
+    sudo ./attack_ntmp_benchmark >> asus_load.csv
+    sudo ./no_flushing_sampled >> asus_load.csv
+    sudo ./no_flushing_benchmark >> asus_load.csv
+    sudo ./random_sampled >> asus_load.csv
+    sudo ./random_benchmark >> asus_load.csv
+    sudo ./rowhammer_test_sampled >> asus_load.csv
+    sudo ./rowhammer_test_benchmark >> asus_load.csv
+    sudo ./double_sided_rowhammer_sampled >> asus_load.csv
+    sudo ./double_sided_rowhammer_benchmark >> asus_load.csv
+    sudo ./stream_sampled >> asus_load.csv
+    sudo ./stream_benchmark >> asus_load.csv
     NUM_RUNS=$(( $NUM_RUNS - 1 ))
 done
 
